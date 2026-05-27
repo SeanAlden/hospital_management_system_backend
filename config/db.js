fd2b09cc45e420c5f824e9d2b1f7c2dd60dc3c15
Sean Alden <c14210052@john.petra.ac.id>
2026-05-27 12:11:36 +0700

    update db

--- config/db.js
+++ config/db.js
@@ -41,39 +41,39 @@
 
 // module.exports = db;
 
-// const mysql = require('mysql');
+const mysql = require('mysql');
 
-// const db = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-//   port: process.env.DB_PORT,
-//   connectionLimit: 10
-// });
-
-// db.getConnection((err, connection) => {
-//   if (err) {
-//     console.error("❌ Database connection failed:", err.message);
-//   } else {
-//     console.log("✅ Connected to MySQL pool");
-//     connection.release();
-//   }
-// });
-
-// module.exports = db;
-
-const mysql = require('mysql2/promise');
-
-const pool = mysql.createPool({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  connectionLimit: 10
 });
 
-module.exports = pool;
+db.getConnection((err, connection) => {
+  if (err) {
+    console.error("❌ Database connection failed:", err.message);
+  } else {
+    console.log("✅ Connected to MySQL pool");
+    connection.release();
+  }
+});
+
+module.exports = db;
+
+// const mysql = require('mysql2/promise');
+
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0
+// });
+
+// module.exports = pool;
